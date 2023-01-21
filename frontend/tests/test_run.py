1b84232cdcd3dfc7a9e7dc948032bf1439442af9
Gbenga Vincent Akinwande <akinwandegbengavincent@gmail.com>
2023-01-21 04:50:22 +0100

    added test cases for frontend

--- frontend/tests/test_run.py
+++ frontend/tests/test_run.py
@@ -1,25 +1,74 @@
 import pytest
+import subprocess
 import requests
-import time
+import time,warnings
 import tempfile
 import subprocess
 from requests_html import HTMLSession
-import warnings
+import tempfile
+import subprocess
+import pytest
+import subprocess
+import requests
+import time
+
+# from bs4 import BeautifulSoup, FeatureNotFound
+
+# @pytest.fixture(scope='session')
+# def streamlit_server():
+#     with tempfile.TemporaryFile() as stdout_file, tempfile.TemporaryFile() as stderr_file:
+#         proc = subprocess.Popen(["streamlit", "run", "app.py", "--server.address=0.0.0.0", "--server.port=8501","--server.enableCORS=false", "--server.headless=True"], stdout=stdout_file, stderr=stderr_file)
+#         time.sleep(2)
+#         yield stdout_file, stderr_file
+#         proc.terminate()
+
+# def test_server(streamlit_server):
+#     response = requests.get('http://localhost:8501')
+#     assert 200 == response.status_code
+
+# @pytest.fixture
+# def capsys():
+#     """Return capsys fixture"""
+#     import sys
+#     from io import StringIO
+#     sys.stdout = StringIO()
+#     sys.stderr = StringIO()
+#     yield sys.stdout, sys.stderr
+#     sys.stdout.seek(0)
+#     sys.stderr.seek(0)
+#     stdout = sys.stdout.read()
+#     stderr = sys.stderr.read()
 
 # from bs4 import BeautifulSoup, FeatureNotFound
 
 @pytest.fixture(scope='session')
 def streamlit_server():
     with tempfile.TemporaryFile() as stdout_file, tempfile.TemporaryFile() as stderr_file:
-        proc = subprocess.Popen(["streamlit", "run", "app.py", "--server.address=0.0.0.0", "--server.port=8501","--server.enableCORS=false", "--server.headless=True"], stdout=stdout_file, stderr=stderr_file)
+        proc = subprocess.Popen(["streamlit", "run", "app.py", "--server.address=0.0.0.0", "--server.port=8501", "--server.headless=True"], stdout=stdout_file, stderr=stderr_file)
         time.sleep(2)
         yield stdout_file, stderr_file
         proc.terminate()
+        stdout_file.seek(0)
+        stderr_file.seek(0)
+        stdout = stdout_file.read().decode()
+        stderr = stderr_file.read().decode()
 
 def test_server(streamlit_server):
     response = requests.get('http://localhost:8501')
     assert 200 == response.status_code
 
+# @pytest.fixture
+# def capsys():
+#     """Return capsys fixture"""
+#     import sys
+#     from io import StringIO
+#     sys.stdout = StringIO()
+#     sys.stderr = StringIO()
+#     yield sys.stdout, sys.stderr
+#     sys.stdout.seek(0)
+#     sys.stderr.seek(0)
+#     stdout = sys.stdout.read()
+#     stderr = sys.stderr.read()
 
 def test_app_nameerror(streamlit_server):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -36,7 +85,6 @@ def test_app_nameerror(streamlit_server):
     
 def test_app_syntaxerror(capsys, streamlit_server):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
-
     time.sleep(1)
     session = HTMLSession()
     response = session.get('http://localhost:8501')
@@ -45,4 +93,5 @@ def test_app_syntaxerror(capsys, streamlit_server):
     stderr_file.seek(0)
     stdout = stdout_file.read().decode()
     stderr = stderr_file.read().decode()
+    print(stderr)
     assert "SyntaxError" not in stderr
