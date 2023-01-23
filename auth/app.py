9412290660c77635639986388c75d253f454503e
Gbenga Vincent Akinwande <akinwandegbengavincent@gmail.com>
2023-01-23 01:07:18 +0100

    structuring auth project folder

--- auth/app.py
+++ auth/app.py
@@ -1,22 +1,30 @@
 from flask import Flask, jsonify
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
-import jwt
+import jwt, os
 from datetime import datetime, timedelta
 from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
+
+# Load secrets
+secret_key = os.environ.get('APP_SECRET_KEY')  # replace with your own secret key
 
 # Connect to the MongoDB server
-client = MongoClient('mongodb://localhost:27017/')
+mongo_url = os.environ.get('MONGO_URL')
+mongo_username = os.environ.get('MONGO_INITDB_ROOT_USERNAME')
+mongo_password = os.environ.get('MONGO_INITDB_ROOT_PASSWORD')
+client = MongoClient(mongo_url)
 
 # Connect to the "auth_db" database and the "revoked_tokens" collection
-db = client["auth_db"]
+db = client['auth_db']
+db.authenticate(mongo_username, mongo_password)
+
+# Get revoked tokens
 revoked_tokens = db["revoked_tokens"]
 
 app = Flask(__name__)
 api = Api(app)
-
-secret_key = 'secret_key'  # replace with your own secret key
-revoked_tokens = set()  # keep track of revoked tokens
 
 class GetToken(Resource):
     def post(self):
@@ -35,10 +43,10 @@ class GetToken(Resource):
         return {'token': token.decode('utf-8')}, 200
     
 
-
-@app.route("/health")
-def health():
-    return jsonify(status="UP")
+class HealthCheck(Resource):
+    def get(self):
+        return jsonify(status="UP"), 200
+    
 
 class VerifyToken(Resource):
     def post(self):
@@ -100,7 +108,11 @@ class RevokeToken(Resource):
         else:
             return {'error': 'Authorization header is missing.'}, 401
                 
-api.add_resource(VerifyToken, '/verify')
+api.add_resource(GetToken, '/get_token')
+api.add_resource(VerifyToken, '/verify_token')
+api.add_resource(RefreshToken, '/refresh_token')
+api.add_resource(RevokeToken, '/revoke_token')
+api.add_resource(HealthCheck, '/health')
 
 
 if __name__=="__main__":
